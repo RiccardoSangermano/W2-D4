1bc34dad52126b9d7fcc23eaa67ddde9cab85b17
RiccardoSangermano <riccardosangermano297@gmail.com>
2025-02-13 16:48:40 +0100

    final

---
+++ D4.js
@@ -0,0 +1,126 @@
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+const l1= 20
+const l2= 30
+function area=(l1, l2){
+return 600
+}
+    
+const total= area= 600
+console.log(risultato)
+
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+const crazySum= function (6, 10) {
+    if(6 !== 10 === "somma",16){
+        return "somma"
+    }
+        else(6 === 10 === "moltiplicazione", 16*3){
+            return " moltiplicazione"
+        }
+
+}
+const operation = crazySum (6 + 10) || (6 * 10)
+console.log(somma)
+console.log(moltiplicazione)
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+const boundary = function(50){
+    const risultato = if(50 >= 20 && 50 <= 100 = true){
+        return true
+    }
+        else(50==400 = true)–[ 
+            return true
+        ]
+    
+}
+const risultato = boundary(if(50 >= 20 && 50 <= 100 = true)
+    else(50==400 = true))
+console.log("conpreso tra 20 e 100")
+console.log("uguale a 400")
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+const epify= function epify("web developers"){
+    
+    const p.push("Epicode")
+    return "Epicode web developers"
+
+}
+const complete= epify.p("Epicode")
+epify="Epicode web developer"
+console.log(benvenuto)
+
+
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+ fuction check3and7(){
+    for( 27, "multiplo di 3" || "multiplo di 7", i++ )
+
+}
+
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+const sea= function reverseString(str){
+    return str
+
+}
+reverseStrin("hello")
+
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+const animal= fuction upperFirst("cane _ gatto _ coniglio") #{
+    upperFirst.toUpperCase[0,7, 15 ]
+
+}
+const a= const animal.upperFirst.toUpperCase
+console.log(Cane, Gatto, Coniglio)
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+const cut= fuction cutString("ora della colazione"){
+cutString.slice(0, 19)
+return 
+}
+const cut=cutString.slice(0, 19)
+
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
